28000890122f858531c14aef3a8d64f73b6aa178
Mfp2010 <mfpereira.pt2010@gmail.com>
2026-06-30 11:10:28 +0100

    Add GET and POST

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from player import Player  # importa a classe
 from task import Task  # importa a classe
+from game import Game
 
 app = Flask(__name__)
 CORS(app)
@@ -19,13 +20,46 @@ players = [
     Player("Maria", tasks, 1),
 ]
 
+game = Game(0)
+
 @app.route('/players')
 def get_players():
     return jsonify([p.to_dict() for p in players])
 
-@app.route('/tasks')
-def get_tasks():
-    return jsonify([t.to_dict() for t in tasks])
+@app.route("/game",methods=['GET','POST'])
+def game_status():
+    global game
+    if request.method == "GET":
+        return jsonify({
+            "state": game.get_state()
+        })
+    if request.method == 'POST':
+        data = request.get_json()
+        print(data)
+        game = Game(data["state"])
+        print(game.get_state())
+    return jsonify({
+        "state": game.get_state()
+    })
+
+@app.route("/tasks",methods=['GET','POST'])
+def game_status():
+    if request.method == "GET":
+        for task in tasks:
+            return jsonify({
+                "name": task.get_state(),
+                "completed": task.get_state(),
+                "place": task.get_state(),
+                "objective": task.get_state()
+            })
+    if request.method == 'POST':
+        data = request.get_json()
+        print(data)
+        game = Game(data["state"])
+        print(game.get_state())
+    return jsonify({
+        "state": game.get_state()
+    })
 
 
 @app.route("/")
